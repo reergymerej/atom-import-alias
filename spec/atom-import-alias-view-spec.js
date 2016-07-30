a48392fcc3fcfc0946d594add7e70c1d3cdda670
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-07-29 22:20:44 -0500

    initial commit

---
+++ spec/atom-import-alias-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomImportAliasView from '../lib/atom-import-alias-view';
+
+describe('AtomImportAliasView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
